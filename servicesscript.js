cc326ed6222b42033852f9dfbb31181626e7c86d
pratd <39798358+pratd@users.noreply.github.com>
2019-11-05 15:51:38 +0100

    final final commit

--- servicesscript.js
+++ servicesscript.js
@@ -1,23 +1,15 @@
 $(document).ready(function(){
-    var $modalgallery, $img, $modalImg;
-
-   $modalgallery= $('.modalgallery');
-   $img = $("myImg");
-   $modalImg= $("img01");
-   $img.onclick = function(){
-       $modalgallery.style.display = "block";
-       $modalImg.src = this.src;
-   }
-})
-
-/*var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}*/
+  var colap = document.getElementsByClassName("collapsible") ;
+  var i;
+  for(i=0; i<colap.length; i++) {
+    colap[i].addEventListener("click", function(){
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
+})//this is for running the more detailed sections
